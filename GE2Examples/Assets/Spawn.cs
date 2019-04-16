047278fd4108b8deeee32e420c6ac68e117989c0
JackBurleigh508 <33128868+JackBurleigh508@users.noreply.github.com>
2019-04-16 13:12:53 +0100

    Fighter Spawner created

---
+++ GE2Examples/Assets/Spawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject fighters;
+    public int numberOfFighters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i <= numberOfFighters; i++)
+        {
+            Instantiate(fighters);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
